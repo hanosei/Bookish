c8faa4fc6ab4947e3d2230bd9c612a3158f0f471
Hannah Osei <hannah.osei@softwire.com>
2025-06-04 19:08:11 +0100

    search bar on homepage

--- obj/Debug/net8.0/Bookish.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bookish.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookish")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e50a93cf3bedbea5fbb92abc2c335a860df3ebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+926cbd7c649c79c9516fc9f7454d0192a567e3b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookish")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookish")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
